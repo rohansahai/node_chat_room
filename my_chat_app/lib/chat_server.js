808bc6c2462ce4444c1e67ea2156516b586f7f34
Rohan Sahai <rsahai91@gmail.com>
2014-05-20 14:55:56 -0700

    Socket is listening

---
+++ my_chat_app/lib/chat_server.js
@@ -0,0 +1,18 @@
+var socketIo = require('socket.io');
+
+function createChat (server) {
+  var io = socketIo.listen(server);
+  io.sockets.on('connection', function (socket) {
+    //io.sockets.emit('message', { text: 'this is the text' })
+    socket.on('message', function (data){
+      console.log(data);
+      io.sockets.emit('message', { text: data })
+    });
+  });
+};
+
+module.exports = createChat;
+
+// our application, we want listen for a message event and respond by broadcasting the message text to the chatroom. Use the same socket.on('event', callback) pattern to set a callback for the message event that will be raised when a message is sent.
+//
+// For now, all this callback has to do is emit the received message to all the connected sockets.
--- my_chat_app/server.js
+++ my_chat_app/server.js
@@ -1,12 +1,30 @@
-var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var router = require('./router');
+var createChat = require('./lib/chat_server');
+var app = require('http').createServer(handler)
 
-http.createServer(function (request, response){
-  router(request, response);
+// http.createServer(function (request, response){
+//   router(request, response);
+//
+// }).listen(8080);
+//
+// console.log('Server Running, great');
 
-}).listen(8080);
+// app.listen(80);
 
-console.log('Server Running, great');
+function handler (req, res) {
+  fs.readFile(__dirname + '/public/index.html',
+    function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading index.html');
+      }
+
+      res.writeHead(200);
+      res.end(data);
+    });
+}
+
+createChat(app);
