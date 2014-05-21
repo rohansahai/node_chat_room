36228f5f65364a3c04f50b7aed3581a47bc0fdf6
Rohan Sahai <rsahai91@gmail.com>
2014-05-20 17:57:13 -0700

    updating nick names and showing user list

--- my_chat_app/lib/chat_server.js
+++ my_chat_app/lib/chat_server.js
@@ -7,6 +7,9 @@ function createChat (server) {
   io.sockets.on('connection', function (socket) {
     guestNumber++;
     guests[socket.id] = "guest" + guestNumber;
+
+    io.sockets.emit('nicknamesList', { nameList: getNickNames() });
+
     socket.on('message', function (data){
       io.sockets.emit('message',
                       { text: guests[socket.id] + ": " + data })
@@ -15,8 +18,8 @@ function createChat (server) {
     socket.on('nicknameChangeRequest', function (data) {
       //check valid nickname, if valid send success true message, if not success false message
       var taken = false;
-      for (var name in guests) {
-        if (guests[name] == data) {
+      for (var socketId in guests) {
+        if (guests[socketId] == data) {
           taken = true;
         }
       }
@@ -24,16 +27,21 @@ function createChat (server) {
       if (!taken) {
         guests[socket.id] = data;
         socket.emit('nicknameChangeResult', { success: true, message: "Nickname updated" });
+        io.sockets.emit('nicknamesList', { nameList: getNickNames() });
       } else {
         socket.emit('nicknameChangeResult', { success: false, message: "Can't use that one" });
       }
 
     })
   });
-
-  // io.sockets.on('disconnect', function () {
-  //   guestNumber--;
-  // })
 };
+
+function getNickNames() {
+  var nameList = [];
+  for (var socketId in guests) {
+    nameList.push(guests[socketId]);
+  }
+  return nameList;
+}
 
 module.exports = createChat;
